acab9cc17694517c903bbe9ca2216bb7bd7b580b
jabonchan <jabonchan.soap@gmail.com>
2026-06-23 15:50:33 -0400

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,24 @@
+#include "windows.h"
+#include <cstdint>
+#include "./lib/lz4.h"
+
+extern "C" __declspec(dllexport) int LZ4_Compress(
+    const uint8_t* src,
+    int srcSize,
+    uint8_t* dst,
+    int dstCapacity
+) {
+    if (!src || !dst || srcSize < 0 || dstCapacity < 0)
+        return 0;
+
+    return LZ4_compress_default(
+        reinterpret_cast<const char*>(src),
+        reinterpret_cast<char*>(dst),
+        srcSize,
+        dstCapacity
+    );
+}
+
+extern "C" __declspec(dllexport)  int LZ4_MaxCompressedSize(int srcSize) {
+    return LZ4_compressBound(srcSize);
+}
